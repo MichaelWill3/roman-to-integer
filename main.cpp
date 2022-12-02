7843f395e6fdcd0a51549f0d4a74934611da0cb0
MichaelWill <michaelwill3@gmail.com>
2022-12-02 16:12:47 +0100

    Move map inside function

--- main.cpp
+++ main.cpp
@@ -15,6 +15,17 @@ std::map<char,unsigned> romanNumeralToValue =
 };
 
 int romanToInt(const std::string &romanNumber) {
+    static const std::map<char,unsigned> romanNumeralToValue = 
+    {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000}
+    };
+    
     unsigned result = 0;
     unsigned previousValue = 9999;
     for(auto character : romanNumber)
